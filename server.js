86432033d10bcbeb82db4f82d2d3ebddada07be4
Corné van Straten <vanstratenc@gmail.com>
2018-12-07 22:19:19 +0100

    add 0.0.0.0 to server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT | 3000;
+const hostname = process.env.HOSTNAME
 
 const server = http.createServer(app);
 
-server.listen(":"+port, () => {
+server.listen(port, "0.0.0.0", () => {
   console.log('radius api started and is running at ' + port)
 });
