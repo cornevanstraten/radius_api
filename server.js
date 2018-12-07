fb655904b47d3a96eb5c9a5f5a458053fb4ddd08
Corné van Straten <vanstratenc@gmail.com>
2018-12-07 21:52:18 +0100

    change server text to find port

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT | 3000;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log("radius API server has started")
+  console.log('radius api started and is running at ' + port)
 });
