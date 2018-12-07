5cd40a057c130f9bd91b03a7e4a6acf835f33024
Corné van Straten <vanstratenc@gmail.com>
2018-12-07 22:26:43 +0100

    another attempt at bugfix

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 const hostname = process.env.HOSTNAME
 
 const server = http.createServer(app);
 
 server.listen(port, "0.0.0.0", () => {
-  console.log('radius api started and is running at ' + port)
+  console.log('radius api started and is running at port ' + port + '; hostname: ' + hostname)
 });
